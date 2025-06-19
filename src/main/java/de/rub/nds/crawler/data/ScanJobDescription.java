220673b089b22a08fc7979c56d0c71a2ec0eeabe
Robert Merget <robert.merget@tii.ae>
2025-06-19 05:49:51 +0000

    Fix SE_NO_SERIALVERSIONID SpotBugs issue in ScanJobDescription
    Added serialVersionUID field to ScanJobDescription class to address SpotBugs warning.

--- src/main/java/de/rub/nds/crawler/data/ScanJobDescription.java
+++ src/main/java/de/rub/nds/crawler/data/ScanJobDescription.java
@@ -15,6 +15,8 @@ import java.util.Optional;
 
 public class ScanJobDescription implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
     private final ScanTarget scanTarget;
 
     // Metadata
