cd33a04874277308c35b05454bd69edb2f33a7b4
Robert Merget <robert.merget@tii.ae>
2025-06-19 11:01:41 +0000

    Add Javadoc to CancellableFuture public methods

--- src/main/java/de/rub/nds/crawler/util/CancellableFuture.java
+++ src/main/java/de/rub/nds/crawler/util/CancellableFuture.java
@@ -18,6 +18,11 @@ public class CancellableFuture<V> implements RunnableFuture<V> {
     private final RunnableFuture<V> innerFuture;
     private final Semaphore resultWritten = new Semaphore(0);
 
+    /**
+     * Creates a new CancellableFuture that will execute the given Callable.
+     *
+     * @param callable the callable task to execute
+     */
     public CancellableFuture(Callable<V> callable) {
         innerFuture =
                 new FutureTask<>(
@@ -29,6 +34,13 @@ public class CancellableFuture<V> implements RunnableFuture<V> {
                         });
     }
 
+    /**
+     * Creates a new CancellableFuture that will execute the given Runnable and return the specified
+     * result upon completion.
+     *
+     * @param runnable the runnable task to execute
+     * @param res the result to return upon successful completion
+     */
     public CancellableFuture(Runnable runnable, V res) {
         innerFuture =
                 new FutureTask<>(
@@ -40,21 +52,48 @@ public class CancellableFuture<V> implements RunnableFuture<V> {
                         });
     }
 
+    /**
+     * Attempts to cancel execution of this task.
+     *
+     * @param b {@code true} if the thread executing this task should be interrupted; otherwise,
+     *     in-progress tasks are allowed to complete
+     * @return {@code false} if the task could not be cancelled, typically because it has already
+     *     completed normally; {@code true} otherwise
+     */
     @Override
     public boolean cancel(boolean b) {
         return innerFuture.cancel(b);
     }
 
+    /**
+     * Returns {@code true} if this task was cancelled before it completed normally.
+     *
+     * @return {@code true} if this task was cancelled before it completed
+     */
     @Override
     public boolean isCancelled() {
         return innerFuture.isCancelled();
     }
 
+    /**
+     * Returns {@code true} if this task completed.
+     *
+     * @return {@code true} if this task completed
+     */
     @Override
     public boolean isDone() {
         return innerFuture.isDone();
     }
 
+    /**
+     * Waits if necessary for the computation to complete, and then retrieves its result. If the
+     * task was cancelled, this method will still return the result that was computed before
+     * cancellation.
+     *
+     * @return the computed result
+     * @throws InterruptedException if the current thread was interrupted while waiting
+     * @throws ExecutionException if the computation threw an exception
+     */
     @Override
     public V get() throws InterruptedException, ExecutionException {
         try {
@@ -65,6 +104,18 @@ public class CancellableFuture<V> implements RunnableFuture<V> {
         }
     }
 
+    /**
+     * Waits if necessary for at most the given time for the computation to complete, and then
+     * retrieves its result. If the task was cancelled, this method will still return the result
+     * that was computed before cancellation.
+     *
+     * @param l the maximum time to wait
+     * @param timeUnit the time unit of the timeout argument
+     * @return the computed result
+     * @throws InterruptedException if the current thread was interrupted while waiting
+     * @throws ExecutionException if the computation threw an exception
+     * @throws TimeoutException if the wait timed out
+     */
     @Override
     public V get(long l, @NonNull TimeUnit timeUnit)
             throws InterruptedException, ExecutionException, TimeoutException {
@@ -78,6 +129,7 @@ public class CancellableFuture<V> implements RunnableFuture<V> {
         }
     }
 
+    /** Sets this Future to the result of its computation unless it has been cancelled. */
     @Override
     public void run() {
         innerFuture.run();
